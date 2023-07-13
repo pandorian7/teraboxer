0beb527c33a072758507e6160ce2040de62ec5b2
Yasith Piyarathne <yasithpiyarathne@gmail.com>
2023-07-14 01:28:43 +0530

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='TeraBoxer',
+    version='1.0.0',
+    packages=['teraboxer'],
+    url='https://github.com/yasithpiyarathne/teraboxer',
+    license='',
+    author='Yasith Piyarathne',
+    author_email='yasithpiyarathne@gmail.com',
+    description='python uploader for terabox.com'
+)
