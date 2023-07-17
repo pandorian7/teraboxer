9e8126677ceb44e44ec4d74cd0d0d407550e3c1a
Yasith Piyarathne <yasithpiyarathne@gmail.com>
2023-07-17 15:28:10 +0530

    added __main__.py

--- teraboxer/__main__.py
+++ teraboxer/__main__.py
@@ -1,3 +1,5 @@
+from tqdm.utils import CallbackIOWrapper
+from tqdm import tqdm
 import argparse
 import os
 
@@ -9,6 +11,11 @@ parser.add_argument("file_path", help="path of the file that need to be uploaded
 parser.add_argument('--ndus',default=None, help="ndus key from terabox cookies")
 
 args = parser.parse_args()
-print(args.ndus or os.environ['TERABOX_KEY'])
 tb = TeraBox(args.ndus or os.environ['TERABOX_KEY'])
-tb.upload_file(args.file_path)
+file = args.file_path
+file_size = os.stat(file).st_size
+base_name = os.path.basename(file)
+with open(file, 'rb') as f:
+    with tqdm(total=file_size, unit_scale=True, unit='B', unit_divisor=1024, desc=base_name) as progress:
+        wrapped_file = CallbackIOWrapper(progress.update, f, "read")
+        tb.upload_file_obj(wrapped_file,base_name,file_size)
